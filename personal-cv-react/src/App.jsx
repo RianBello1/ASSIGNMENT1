d1132b6610ab6235e3f88732079131ca5990a07d
rianbello1 <bello.riannathaniel@gmail.com>
2026-02-26 15:53:44 +0800

    test

--- personal-cv-react/src/App.jsx
+++ personal-cv-react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
-import About from "./components/About";
+import About from "./components/about";
 import Skills from "./components/Skills";
 import Education from "./components/Education";
 import Contact from "./components/Contact";

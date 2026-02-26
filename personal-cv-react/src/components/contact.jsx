da218e5dc7379f273a811d777fa6715c113799c5
rianbello1 <bello.riannathaniel@gmail.com>
2026-02-26 15:51:04 +0800

    test

--- personal-cv-react/src/components/contact.jsx
+++ personal-cv-react/src/components/contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
 
   return (
     <section className="card">
-      <h2>Contact Me</h2>
+      <h2>Contact Mee</h2>
       <form onSubmit={handleSubmit}>
         <input type="text" id="name" placeholder="Name" />
         <input type="email" id="email" placeholder="Email" />

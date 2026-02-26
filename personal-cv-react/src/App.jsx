bf2453fe66d38f350cb86ba93c57319a65141a2b
rianbello1 <bello.riannathaniel@gmail.com>
2026-02-26 16:00:30 +0800

    WSAT-M5: Migrated CV to React

--- personal-cv-react/src/App.jsx
+++ personal-cv-react/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import Header from "./components/Header";
+import Header from "./components/header";
 import About from "./components/about";
-import Skills from "./components/Skills";
-import Education from "./components/Education";
-import Contact from "./components/Contact";
+import Skills from "./components/skills";
+import Education from "./components/education";
+import Contact from "./components/contact";
 import './index.css';
 
 function App() {
